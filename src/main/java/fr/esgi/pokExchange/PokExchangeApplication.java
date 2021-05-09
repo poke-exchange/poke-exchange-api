3f40573f65257643dc527c9a6081e5753aae9e66
Antonin Texier <antonin.texier@gmail.com>
2021-05-09 20:15:56 +0200

    Init project.

---
+++ src/main/java/fr/esgi/pokExchange/PokExchangeApplication.java
@@ -0,0 +1,13 @@
+package fr.esgi.pokExchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokExchangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/esgi/pokExchange/PokExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.esgi.pokExchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
