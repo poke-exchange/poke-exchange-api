137b9b049884e14598fab4d640555f50dff437ce
Antonin Texier <antonin.texier@gmail.com>
2021-07-04 01:11:38 +0200

    feat(infrastructure): Rename UserCard entity dependency.

--- src/main/java/fr/esgi/poke_exchange_api/infrastructure/pokecards/UserCardRepository.java
+++ src/main/java/fr/esgi/poke_exchange_api/infrastructure/pokecards/UserCardRepository.java
@@ -1,10 +1,10 @@
 package fr.esgi.poke_exchange_api.infrastructure.pokecards;
 
-import fr.esgi.poke_exchange_api.infrastructure.pokecards.models.UserCardEntity;
+import fr.esgi.poke_exchange_api.infrastructure.pokecards.models.CollectedCardEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface UserCardRepository extends JpaRepository<UserCardEntity, UUID> { }
+public interface UserCardRepository extends JpaRepository<CollectedCardEntity, UUID> { }
