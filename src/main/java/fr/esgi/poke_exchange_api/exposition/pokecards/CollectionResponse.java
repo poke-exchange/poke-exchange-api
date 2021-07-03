39d15fb633c0cfc2a2a6438f28d3705eef1ad05e
Antonin Texier <antonin.texier@gmail.com>
2021-07-04 01:13:01 +0200

    feat(domain): Create collection response model.

---
+++ src/main/java/fr/esgi/poke_exchange_api/exposition/pokecards/CollectionResponse.java
@@ -0,0 +1,17 @@
+package fr.esgi.poke_exchange_api.exposition.pokecards;
+
+import fr.esgi.poke_exchange_api.domain.pokecards.models.CollectedCard;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@RequiredArgsConstructor
+public class CollectionResponse {
+
+    private final UUID userId;
+    private final List<CollectedCard> cards;
+
+}
