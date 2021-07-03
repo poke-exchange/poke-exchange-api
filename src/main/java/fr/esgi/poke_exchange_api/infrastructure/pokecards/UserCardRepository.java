0ae62dcebb7ef582b453d8831622cda0de69d246
Antonin Texier <antonin.texier@gmail.com>
2021-07-03 20:50:29 +0200

    feat(infrastructure): Add @Repository annotation.

--- src/main/java/fr/esgi/poke_exchange_api/infrastructure/pokecards/UserCardRepository.java
+++ src/main/java/fr/esgi/poke_exchange_api/infrastructure/pokecards/UserCardRepository.java
@@ -2,7 +2,9 @@ package fr.esgi.poke_exchange_api.infrastructure.pokecards;
 
 import fr.esgi.poke_exchange_api.infrastructure.pokecards.models.UserCardEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface UserCardRepository extends JpaRepository<UserCardEntity, UUID> { }
