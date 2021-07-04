3f1cf49721f56eccdacc06cc3d4abe70c3597dd2
Antonin Texier <antonin.texier@gmail.com>
2021-07-04 16:53:45 +0200

    feat(exposition): Update a user card from its collection feature.

--- src/main/java/fr/esgi/poke_exchange_api/exposition/pokecards/CollectionController.java
+++ src/main/java/fr/esgi/poke_exchange_api/exposition/pokecards/CollectionController.java
@@ -1,9 +1,12 @@
 package fr.esgi.poke_exchange_api.exposition.pokecards;
 
+import fr.esgi.poke_exchange_api.domain.pokecards.exceptions.PokeCardNotFoundException;
+import fr.esgi.poke_exchange_api.domain.pokecards.models.CollectedCard;
 import fr.esgi.poke_exchange_api.domain.pokecards.services.CollectionService;
 import fr.esgi.poke_exchange_api.exposition.pokecards.models.Collection;
 import fr.esgi.poke_exchange_api.exposition.pokecards.validators.CollectionValidator;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -37,5 +40,23 @@ public class CollectionController {
         this.collectionService.saveCollection(request);
 
         return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/user/{id}")
+    public ResponseEntity<?> updateUserCardQuantity(
+            @PathVariable("id") UUID userId, @RequestBody CollectedCard card)
+    {
+        try {
+            var isUpdated = this.collectionService.updateCardQuantity(userId, card);
+
+            if (!isUpdated) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+            }
+
+            return ResponseEntity.ok().build();
+
+        } catch (PokeCardNotFoundException exception) {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
