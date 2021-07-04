2720e611ed276f713fdc1aa43b5a9b7a9a8f64cf
Antonin Texier <antonin.texier@gmail.com>
2021-07-04 16:03:32 +0200

    refactor(infrastructure): Rename entity.

--- src/main/java/fr/esgi/poke_exchange_api/infrastructure/pokecards/CollectedCardsRepository.java
+++ src/main/java/fr/esgi/poke_exchange_api/infrastructure/pokecards/CollectedCardsRepository.java
@@ -1,10 +1,10 @@
 package fr.esgi.poke_exchange_api.infrastructure.pokecards;
 
-import fr.esgi.poke_exchange_api.infrastructure.pokecards.models.CollectedCardsEntity;
+import fr.esgi.poke_exchange_api.infrastructure.pokecards.models.CollectedCardEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface CollectedCardsRepository extends JpaRepository<CollectedCardsEntity, UUID> { }
+public interface CollectedCardsRepository extends JpaRepository<CollectedCardEntity, UUID> { }
